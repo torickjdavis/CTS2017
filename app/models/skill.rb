c83be1b066e3461378f03448e0a03d27dee65e73
Torick Davis <torickjdavis@gmail.com>
2017-07-06 16:46:53 +0000

    Implemented Concern for managing Image Data as Placeholder

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,7 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :percent_utilized
+  def set_defaults #make defaults in model (here) or in a migration
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+  end
 end
