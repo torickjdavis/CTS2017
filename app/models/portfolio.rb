b39522284e32fffbbd4054d3627e9ce59dc68d78
Torick Davis <torickjdavis@gmail.com>
2017-07-06 15:01:19 +0000

    Implemented Custom Scopes for Portfolio Items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
+  #Validation
   validates_presence_of :title, :body, :main_image, :thumb_image
+  #Two ways of making custom scopes
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+  
+  scope :ruby_on_rails, -> {where(subtitle: 'Ruby on Rails')}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     end
   end
   
+  get 'angular-items', to: 'portfolios#angular'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
