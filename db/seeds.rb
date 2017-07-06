ea6e3ced2420b610ba5d6e070443d04d090a5da9
Torick Davis <torickjdavis@gmail.com>
2017-07-06 16:59:16 +0000

    Integrated Technologies Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,3 +47,14 @@ end
     )
 end
 puts "9 portfolio items created"
+
+3.times do |technology|
+  # Technology.create!(
+  #   name: "Technology #{technology}",
+  #   portfolio_id: Portfolio.last.id
+  #   )
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+    )
+end
+puts "3 technologies created"
