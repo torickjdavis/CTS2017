56ec4888a42a83e749e8d279def69160c6caabb4
Torick Davis <torickjdavis@gmail.com>
2017-08-03 03:47:26 +0000

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style #style is a class as a string
     if current_user.is_a?(GuestUser)
-      ( link_to "Login", new_user_session_path ) +
-      "<br>".html_safe + 
-      ( link_to "Register", new_user_registration_path )
+      ( link_to "Login", new_user_session_path, class: style ) +
+      "&nbsp;".html_safe + #rather than using a space " ", I used an html entity for the non-breaking space for semantics and browser compatability
+      ( link_to "Register", new_user_registration_path, class: style )
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
   
