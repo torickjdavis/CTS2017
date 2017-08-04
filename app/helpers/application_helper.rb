298d33f92e1a088a365e04959ee2463883e6def5
Torick Davis <torickjdavis@gmail.com>
2017-08-04 18:06:12 +0000

    Fixed Bug with Login Helper method by adding default value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_helper style #style is a class as a string
+  def login_helper style="" #style is a class as a string, and set default
     if current_user.is_a?(GuestUser)
       ( link_to "Login", new_user_session_path, class: style ) +
       "&nbsp;".html_safe + #rather than using a space " ", I used an html entity for the non-breaking space for semantics and browser compatability
